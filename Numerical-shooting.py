fd4f54bb7671abf6e8bb0c44daea9dfe96e6ca5f
BedenPag <60779252+BedenPag@users.noreply.github.com>
2023-02-17 12:00:49 +0000

    Installing an interative kernal program

--- Numerical-shooting.py
+++ Numerical-shooting.py
@@ -6,10 +6,14 @@ Created on Fri Feb  10 11:16:50 2023
 @author: edenpage
 """
 
+#%% Import libraries
+
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 from scipy.optimize import fsolve
+
+#%% Small b value
 
 # Define the predator-prey function where b = 0.1 (lower limit)
 def ode(t, u):
@@ -19,14 +23,14 @@ def ode(t, u):
     d = 0.1
     b = 0.1
     dxdt = x*(1-x) - (a*x*y)/(d + x)
-    dydt = b*y*(1 - y/x)
+    dydt = b*y*(1 - (y/x))
     return np.array([dxdt, dydt])
 
 # Define the time interval
 deltat_max = 0.01
 
 # Define the initial conditions
-u0 = [2, 1]
+u0 = [0.5,0.5]
 
 # Define the step function - using this instead of a scipy solver to get a better understanding of the process
 def RK4(f, t, u, delta_t): 
@@ -42,7 +46,7 @@ def solve_to(f, u0, t0, deltat_max, step_function):
     u = u0
     t_values = [t]
     values = [u]
-    while t <= 30:
+    while t <= 50:
         u = step_function(f, t, u, deltat_max)
         t += deltat_max
         t_values.append(t)
@@ -71,6 +75,8 @@ plt.ylabel('Predator')
 plt.title('Phase-Plane Plot for the Predator-Prey Model with a small b value')
 plt.show()
 
+#%% Large b value
+
 # Define the predator-prey function where b = 0.5 (upper limit)
 def ode(t, u):
     x = u[0] # Number of prey
@@ -81,12 +87,6 @@ def ode(t, u):
     dxdt = x*(1-x) - (a*x*y)/(d + x)
     dydt = b*y*(1 - y/x)
     return np.array([dxdt, dydt])
-
-# Define the time interval
-deltat_max = 0.01
-
-# Define the initial conditions
-u0 = [2, 1]
 
 # Define the step function - using this instead of a scipy solver to get a better understanding of the process
 def RK4(f, t, u, delta_t): 
@@ -102,7 +102,7 @@ def solve_to(f, u0, t0, deltat_max, step_function):
     u = u0
     t_values = [t]
     values = [u]
-    while t <= 30:
+    while t <= 50:
         u = step_function(f, t, u, deltat_max)
         t += deltat_max
         t_values.append(t)
@@ -130,6 +130,8 @@ plt.xlabel('Prey')
 plt.ylabel('Predator')
 plt.title('Phase-Plane Plot for the Predator-Prey Model with a large b value')
 plt.show()
+
+#%% Shooting method
 
 # Phase condition function using the initial conditions
 def phase_condition(u0, u1):
@@ -170,3 +172,5 @@ plt.show()
 
 
 
+
+# %%
