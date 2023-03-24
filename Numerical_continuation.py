f089fbe592df17dc4070bb3b8206bff2a5324621
BedenPag <oc19520@bristol.ac.uk>
2023-03-24 13:59:51 +0000

    new continuation attempt

--- Numerical_continuation.py
+++ Numerical_continuation.py
@@ -1,77 +1,37 @@
 import numpy as np
-from matplotlib import pyplot as plt
-import scipy.integrate
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
 
-# Define the function
 def f(x, c):
     return x**3 - x + c
 
+def continuation(f, x0, c0, step_size, max_steps, solver):
+    results = []
+    for i in range(max_steps):
+        # Update the parameter value
+        c = c0 + i * step_size
 
-# Define the shooting method
-def shooting(ode, x0, par):
-    # Define the time interval
-    t0 = 0
-    tmax = 1
-    deltat_max = 0.01
-    # Define the step function
-    def RK4(ode, t, x, delta_t, par):
-        k1 = ode(x, par)
-        k2 = ode(t + delta_t/2, x + delta_t/2 * k1, par)
-        k3 = ode(t + delta_t/2, x + delta_t/2 * k2, par)
-        k4 = ode(t + delta_t, x + delta_t * k3, par)
-        return x + delta_t/6 * (k1 + 2*k2 + 2*k3 + k4)
-    # Define the solver function
-    def solve_to(ode, x0, t0, deltat_max, step_function, par):
-        t = t0
-        x = x0
-        while t <= tmax:
-            x = step_function(ode, t, x, deltat_max, par)
-            t += deltat_max
-        return x
-    # Solve the ODE
-    x = solve_to(ode, x0, t0, deltat_max, RK4, par)
-    return x
+        # Solve the equation for the current parameter value
+        sol = solver(lambda x: f(x, c), x0)
 
-# Define the natural parameter continuation function
-def continuation(ode, x0, par0, vary_par=0, step_size=0.1, max_steps=100, discretisation=shooting, solver=scipy.optimize.fsolve):
-    # Define the initial conditions
-    x = x0
-    par = par0
-    # Define the step function
-    def step_function(ode, x, par, step_size):
-        par[vary_par] += step_size
-        x = solver(ode, x, par)
-        return x, par
-    # Define the solver function
-    def solve_to(ode, x, par, step_size, step_function):
-        for i in range(max_steps):
-            x, par = step_function(ode, x, par, step_size)
-        return x, par
-    # Solve the ODE
-    x, par = solve_to(ode, x, par, step_size, step_function)
-    return x, par
+        # Store the solution
+        results.append((c, sol[0]))
 
-# Define the initial conditions
-x0 = 0.5
-par0 = 0.5
+        # Update the initial guess for the next iteration
+        x0 = sol[0]
 
-# Plot the solution for values of c between -2 and 2
-c_values = np.linspace(-2, 2, 100)
-x_values = [shooting(f, x0, c) for c in c_values]
+    return results
+
+x0 = 0.0
+solutions = continuation(f, x0, -2, 0.01, 400, fsolve)
+
+c_values = [sol[0] for sol in solutions]
+x_values = [sol[1] for sol in solutions]
+
 plt.plot(c_values, x_values)
+plt.xlabel('c')
+plt.ylabel('x')
 plt.show()
-
-# Perform natural parameter continuation
-results = continuation(f, x0, par0, vary_par=0, step_size=0.1, max_steps=100, discretisation=shooting, solver=scipy.optimize.fsolve)
-
-# Plot the solution
-plt.plot(results[0], results[1])
-plt.show()
-
-
-
-
-
 
 
 
