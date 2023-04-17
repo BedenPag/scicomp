cd53cbcb8141587333f99ef2ae3cae1a9d477218
BedenPag <oc19520@bristol.ac.uk>
2023-04-17 14:22:56 +0100

    Attempt to add Robin to BVP_uberclass

--- BVP_uberclass.py
+++ BVP_uberclass.py
@@ -9,9 +9,10 @@ class Grid:
         self.x = np.linspace(a, b, N-1)
         
 class BoundaryCondition:
-    def __init__(self, kind, value):
+    def __init__(self, kind, value, gamma=1):
         self.kind = kind
         self.value = value
+        self.gamma = gamma
         
     def apply(self, A, b, grid):
         if self.kind == "Dirichlet":
@@ -30,10 +31,19 @@ class BoundaryCondition:
             
             A[-1, -1] = 1 
             A[-1, -2] = -1 
-            b[-1] += self.value 
+            b[-1] += self.value
+
+        elif self.kind == "Robin": 
+            b[0] -= self.value 
+        
+            A[-1, -1] = -2*(1+self.gamma*grid.dx) 
+            A[-1, -2] = 2 
+            b[-1] += self.value
+
+
             
         else:
-            raise ValueError("Unsupported boundary condition. Please choose 'Dirichlet' or 'Neumann'.")
+            raise ValueError("Unsupported boundary condition. Please choose 'Dirichlet' or 'Neumann' or 'Robin'.")
         
 def construct_A_and_b(grid, bc_left, bc_right, kind):
     N = grid.N
@@ -64,7 +74,21 @@ def construct_A_and_b(grid, bc_left, bc_right, kind):
         b = np.zeros(N)
         bc_left.apply(A, b, grid)
         bc_right.apply(A, b, grid)
+
+    elif kind == "Robin":
+        # construct the matrix A
+        A = np.zeros((N-1, N-1))
+        for i in range(0, N-2):
+            A[i, i-1] = 1
+            A[i, i+1] = 1 
+        for i in range(0, N-2):
+            A[i, i] = -2
+        
+        # apply the boundary conditions
+        b = np.zeros(N-1)
+        bc_left.apply(A, b, grid)
+        bc_right.apply(A, b, grid)
         
     else:
-        raise ValueError("Unsupported boundary condition. Please choose 'Dirichlet' or 'Neumann'.")
+        raise ValueError("Unsupported boundary condition. Please choose 'Dirichlet' or 'Neumann' or 'Robin'.")
     return A[1:-1, 1:-1], b[1:-1]
--- BVPsolve.py
+++ BVPsolve.py
@@ -8,7 +8,7 @@ def q(x):
     return np.ones(np.size(x))
 
 # create the finite-difference grid
-grid = Grid(N=101, a=0, b=1)
+grid = Grid(N=21, a=0, b=1)
 dx = grid.dx
 x = grid.x
 
@@ -52,5 +52,26 @@ plt.plot(x, u_exact, 'r', label = 'Exact solution')
 plt.xlabel('x')
 plt.ylabel('u(x)')
 plt.title('Finite-difference solution with Neumann BC')
+plt.legend()
+plt.show()
+
+# Robin boundary condition example
+
+# update the right boundary condition
+bc_right = BoundaryCondition("Robin", 0.0, 1.0)
+
+# update the matrix A and the vector b
+A_DR, b_DR = construct_A_and_b(grid, bc_left, bc_right, "Robin")
+
+# solve the linear system
+u_DR = np.linalg.solve(A_DR, -b_DR - dx**2 * q(x[1:-1]))
+
+# plot the solution and the true solution
+u_exact = 1/2 * x * (1 - x)
+plt.plot(x[1:-1], u_DR, 'o', label = 'Finite-difference solution')
+plt.plot(x, u_exact, 'r', label = 'Exact solution')
+plt.xlabel('x')
+plt.ylabel('u(x)')
+plt.title('Finite-difference solution with Robin BC')
 plt.legend()
 plt.show()
