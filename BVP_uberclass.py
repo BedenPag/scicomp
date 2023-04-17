91b8a9537701a98d19165a373c73e3a973dab738
BedenPag <oc19520@bristol.ac.uk>
2023-04-17 21:28:14 +0100

    added robin

--- BVP_uberclass.py
+++ BVP_uberclass.py
@@ -2,93 +2,81 @@ import numpy as np
 
 class Grid:
     def __init__(self, N, a, b):
+        '''Create a grid with N points between a and b.'''
         self.N = N
         self.a = a
         self.b = b
-        self.dx = (b - a) / (N - 1)
+        self.dx = (b - a) / (N-1)
         self.x = np.linspace(a, b, N-1)
         
 class BoundaryCondition:
-    def __init__(self, kind, value, gamma=1):
+    def __init__(self, kind, value, alpha=1, beta=1, gamma=1, delta=1):
+        '''Parameters:
+        kind: string, either "Dirichlet", "Neumann" or "Robin"
+        value: float, the value of the boundary condition
+        alpha, beta, gamma, delta: floats, the coefficients of the boundary condition
+        '''
         self.kind = kind
         self.value = value
+        self.alpha = alpha
+        self.beta = beta
         self.gamma = gamma
+        self.delta = delta
+        
         
     def apply(self, A, b, grid):
+        '''Parameters:
+        A: numpy array, the matrix A
+        b: numpy array, the vector b
+        grid: Grid object
+
+        Output:
+        A and b are modified according to the boundary condition
+        '''
+
         if self.kind == "Dirichlet":
-            A[0, :] = 0
-            A[0, 0] = 1
-            b[0] = self.value
-            
-            A[-1, :] = 0
-            A[-1, -1] = 1
-            b[-1] = self.value
+            # delete the first row and the last column of A
+            A = np.delete(A, 0, 0)
+            A = np.delete(A, -1, 1)
+            print(A.shape)
+
+            # delete the first element of b
+            b = np.delete(b, 0)
+
+            # modify the last row and the last element of b
+            b[0] = self.alpha
+            b[-1] = self.beta
             
         elif self.kind == "Neumann":
-            A[0, 0] = -1
-            A[0, 1] = 1 
-            b[0] -= self.value 
+            A[-1, -2] = 2
+
+            b[0] = self.alpha
+            b[-1] = 2*self.delta*grid.dx
+
+        elif self.kind == "Robin":
             
-            A[-1, -1] = 1 
-            A[-1, -2] = -1 
-            b[-1] += self.value
-
-        elif self.kind == "Robin": 
-            b[0] -= self.value 
-        
             A[-1, -1] = -2*(1+self.gamma*grid.dx) 
-            A[-1, -2] = 2 
-            b[-1] += self.value
-
-
+            A[-1, -2] = 2
+            b[0] -= self.alpha
+            b[-1] += self.gamma*grid.dx
             
         else:
             raise ValueError("Unsupported boundary condition. Please choose 'Dirichlet' or 'Neumann' or 'Robin'.")
         
-def construct_A_and_b(grid, bc_left, bc_right, kind):
+def construct_A_and_b(grid, bc_left, bc_right):
     N = grid.N
-    if kind == "Dirichlet":
-        # construct the matrix A
-        A = np.zeros((N-1, N-1))
-        for i in range(0, N-2):
-            A[i, i-1] = 1
-            A[i, i+1] = 1 
-        for i in range(0, N-1):
-            A[i, i] = -2
-        
-        # apply the boundary conditions
-        b = np.zeros(N-1)
-        bc_left.apply(A, b, grid)
-        bc_right.apply(A, b, grid)
-    
-    elif kind == "Neumann":
-        # construct the matrix A
-        A = np.zeros((N, N))
-        for i in range(0, N-1):
-            A[i, i-1] = 1
-            A[i, i+1] = 1 
-        for i in range(0, N):
-            A[i, i] = -2
-        A[N-1, N-2] = 1
-        # apply the boundary conditions
-        b = np.zeros(N)
-        bc_left.apply(A, b, grid)
-        bc_right.apply(A, b, grid)
+    # construct the matrix A
+    A = np.zeros((N-1, N-1))
+    for i in range(0, N-2):
+        A[i, i-1] = 1
+        A[i, i+1] = 1 
+    for i in range(0, N-1):
+        A[i, i] = -2
 
-    elif kind == "Robin":
-        # construct the matrix A
-        A = np.zeros((N-1, N-1))
-        for i in range(0, N-2):
-            A[i, i-1] = 1
-            A[i, i+1] = 1 
-        for i in range(0, N-2):
-            A[i, i] = -2
-        
-        # apply the boundary conditions
-        b = np.zeros(N-1)
-        bc_left.apply(A, b, grid)
-        bc_right.apply(A, b, grid)
-        
-    else:
-        raise ValueError("Unsupported boundary condition. Please choose 'Dirichlet' or 'Neumann' or 'Robin'.")
-    return A[1:-1, 1:-1], b[1:-1]
+    # apply the boundary conditions
+    b = np.zeros(N-1)
+    A = bc_left.apply(A, b, grid)
+    print(A)
+    bc_right.apply(A, b, grid)
+    print(A)
+    return A[0:-1, 0:-1], b[0:-1]
--- BVPsolve.py
+++ BVPsolve.py
@@ -1,77 +1,99 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
-# import the Grid, BoundaryCondition, and construct_A_and_b functions
 from BVP_uberclass import Grid, BoundaryCondition, construct_A_and_b
 
-def q(x):
-    return np.ones(np.size(x))
-
-# create the finite-difference grid
-grid = Grid(N=21, a=0, b=1)
-dx = grid.dx
-x = grid.x
-
-# create two Dirichlet boundary conditions
-bc_left = BoundaryCondition("Dirichlet", 0.0)
-bc_right = BoundaryCondition("Dirichlet", 0.0)
-
-# create the matrix A and the vector b
-A, b = construct_A_and_b(grid, bc_left, bc_right, "Dirichlet")
-
-# solve the linear system
-u = np.linalg.solve(A, -b - dx**2 * q(x[1:-1]))
+def solve_dirichlet_bc(N, a, b, alpha, beta, q):
+    # create the finite-difference grid
+    grid = Grid(N=N, a=a, b=b)
+    dx = grid.dx
+    x = grid.x
 
 
-# plot the solution and the true solution
-u_exact = 1/2 * x * (1 - x)
-plt.plot(x[1:-1], u, 'o', label = 'Finite-difference solution')
-plt.plot(x, u_exact, 'r', label = 'Exact solution')
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.title('Finite-difference solution with Dirichlet BC')
-plt.legend()
-plt.show()
+    # create two Dirichlet boundary conditions
+    bc_left = BoundaryCondition("Dirichlet", 0.0, alpha, beta)
+    bc_right = BoundaryCondition("Dirichlet", 0.0, alpha, beta)
 
-# Neumann boundary condition example
+    # create the matrix A and the vector b
+    A, b = construct_A_and_b(grid, bc_left, bc_right)
 
-# update the right boundary condition
-bc_right = BoundaryCondition("Neumann", 0.0)
+    print(A.shape)
+    print(b.shape)
+    print(q(x[1:-1]).shape)
 
-# update the matrix A and the vector b
-A_DN, b_DN = construct_A_and_b(grid, bc_left, bc_right,"Neumann")
+    # solve the linear system
+    u = np.linalg.solve(A, -b - dx**2 * q(x[1:-1]))
 
+    # plot the solution and the true solution
+    u_exact = 1/2 * x * (1 - x)
+    plt.plot(x[1:-1], u, 'o', label = 'Finite-difference solution')
+    plt.plot(x, u_exact, 'r', label = 'Exact solution')
+    plt.xlabel('x')
+    plt.ylabel('u(x)')
+    plt.title('Finite-difference solution with Dirichlet BC')
+    plt.legend()
+    plt.show()
 
-# solve the linear system
-u_DN = np.linalg.solve(A_DN, -b_DN - dx**2 * q(x[1:]))
+def solve_neumann_bc(N, a, b,alpha,beta,delta, q):
+    # create the finite-difference grid
+    grid = Grid(N=N, a=a, b=b)
+    dx = grid.dx
+    x = grid.x
 
-# plot the solution and the true solution
-u_exact = 1/2 * x * (1 - x)
-plt.plot(x[1:], u_DN, 'o', label = 'Finite-difference solution')
-plt.plot(x, u_exact, 'r', label = 'Exact solution')
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.title('Finite-difference solution with Neumann BC')
-plt.legend()
-plt.show()
+    # create one Dirichlet and one Neumann boundary condition
+    bc_left = BoundaryCondition("Dirichlet", 0.0, alpha, beta)
+    bc_right = BoundaryCondition("Neumann", 0.0, alpha, delta)
 
-# Robin boundary condition example
+    # create the matrix A and the vector b
+    A, b = construct_A_and_b(grid, bc_left, bc_right)
 
-# update the right boundary condition
-bc_right = BoundaryCondition("Robin", 0.0, 1.0)
+    print(A.shape)
+    print(b.shape)
+    print(q(x[1:]).shape)
 
-# update the matrix A and the vector b
-A_DR, b_DR = construct_A_and_b(grid, bc_left, bc_right, "Robin")
+    # solve the linear system
+    u = np.linalg.solve(A, -b - dx**2 * q(x[1:]))
 
-# solve the linear system
-u_DR = np.linalg.solve(A_DR, -b_DR - dx**2 * q(x[1:-1]))
+    # plot the solution and the true solution
+    u_exact = 1/2 * x * (1 - x)
+    plt.plot(x[1:], u, 'o', label = 'Finite-difference solution')
+    plt.plot(x, u_exact, 'r', label = 'Exact solution')
+    plt.xlabel('x')
+    plt.ylabel('u(x)')
+    plt.title('Finite-difference solution with Neumann BC')
+    plt.legend()
+    plt.show()
 
-# plot the solution and the true solution
-u_exact = 1/2 * x * (1 - x)
-plt.plot(x[1:-1], u_DR, 'o', label = 'Finite-difference solution')
-plt.plot(x, u_exact, 'r', label = 'Exact solution')
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.title('Finite-difference solution with Robin BC')
-plt.legend()
-plt.show()
+def solve_robin_bc(N, a, b, alpha, beta,gamma, q):
+    # create the finite-difference grid
+    grid = Grid(N=N, a=a, b=b)
+    dx = grid.dx
+    x = grid.x
+
+    # create one Dirichlet and one Robin boundary condition
+    bc_left = BoundaryCondition("Dirichlet", 0.0, alpha, beta)
+    bc_right = BoundaryCondition("Robin", 0.0, alpha, gamma)
+
+    # create the matrix A and the vector b
+    A, b = construct_A_and_b(grid, bc_left, bc_right)
+
+    # solve the linear system
+    u = np.linalg.solve(A, -b - dx**2 * q(x[1:-1]))
+
+    # plot the solution and the true solution
+    u_exact = 1/2 * x * (1 - x)
+    plt.plot(x[1:-1], u, 'o', label = 'Finite-difference solution')
+    plt.plot(x, u_exact, 'r', label = 'Exact solution')
+    plt.xlabel('x')
+    plt.ylabel('u(x)')
+    plt.title('Finite-difference solution with Robin BC')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    N = 21
+    a = 0
+    b = 1
+    q = lambda x: np.ones(np.size(x))
+    solve_dirichlet_bc(N, a, b,1,1, q)
+    solve_neumann_bc(N, a, b,1,1,1, q)
+    solve_robin_bc(N, a, b, 1, 1, 1 ,q)
